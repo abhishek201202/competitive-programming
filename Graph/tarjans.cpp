0d735c33cd4a1f4c7340dfdbc9522cb03100e361
abhishek <abhishekk201202@gmail.com>
2020-07-26 09:28:08 +0530

    Some Frequently used algorithm in competitive programming

--- Graph/tarjans.cpp
+++ Graph/tarjans.cpp
@@ -1,4 +1,4 @@
-// finding bridge in a graph
+// finding Articulation Point && Bridges in a graph
 #include<bits/stdc++.h>
 #define int long long
 #define ff first
@@ -7,19 +7,20 @@
 using namespace std;
 
 const int N = 1e5;
-int n, m, timer, tin[N], low[N], vis[N];
+int n, m, timer, tin[N], low[N], vis[N], AP[N];
 vector<int> adj[N];
-vector<pair<int,int>> bridge;
+vector<pair<int, int>> B;
 
 void tarjans(){
 	timer = 0;
 	memset(tin, 0, sizeof timer);
 	memset(low, 0, sizeof low);
 	memset(vis, 0, sizeof vis);
-	bridge.clear();
+	memset(AP, 0, sizeof AP);
 	function<void(int, int)> dfs = [&](int u, int p){
 		vis[u] = 1;
 		tin[u] = low[u] = timer++;
+		int children = 0;
 		for(auto v : adj[u]){
 			if(v == p) continue;
 			if(vis[v]){
@@ -27,11 +28,18 @@ void tarjans(){
 			}else{
 				dfs(v, u);
 				low[u] = min(low[u], low[v]);
+				// For Bridges
 				if(low[v] > tin[u]){
-					bridge.push_back({u, v});
+					B.push_back({u, v});
+				}
+				// For Articulation point
+				children++;
+				if(low[v] >= tin[u] && p != -1){
+					AP[u] = 1;
 				}
 			}
 		}
+		if(p == -1 && children > 1) AP[u] = 1;
 	};
 	for(int i = 0 ;i < n ; i++){
 		if(!vis[i]) dfs(i, -1);
@@ -39,50 +47,42 @@ void tarjans(){
 }
 
 int32_t main(){
-	cin >> n >> m;
-	for(int i = 0 ;i < m ; i++){
-		int x , y ; cin >> x >> y;
-		adj[x - 1].pb(y - 1);
-		adj[y - 1].pb(x - 1);
-	}
-	tarjans();
-	for(int i = 0; i < bridge.size() ; i++){
-		cout << bridge[i].ff + 1 << " " << bridge[i].ss + 1 << endl;
+	int tt = 1;
+	// cin >> tt;
+	while(tt--){
+		for(int i = 0; i < N; i++){
+			adj[i].clear();
+		}
+		cin >> n >> m;
+		for(int i = 0 ;i < m ; i++){
+			int x , y ; cin >> x >> y;
+			adj[x].pb(y);
+			adj[y].pb(x);
+		}
+		tarjans();
+		int AP_cnt = 0;
+		for(int i = 0; i < n; i++){ 
+			AP_cnt += AP[i];
+		}
+		cout << AP_cnt << endl;
+		for(int i = 0; i < n; i++){
+			if(AP[i]){
+				cout << i << " "; 
+			}
+		}
+		cout << endl << B.size() << endl;
+		for(int i = 0; i < B.size(); i++){
+			if(B[i].ff > B[i].ss) swap(B[i].ff, B[i].ss);
+		}
+		sort(B.begin(), B.end(), [&](pair<int, int> &a, pair<int, int> &b){
+			if(a.ff == b.ff){
+				return a.ss < b.ss;
+			}else{
+				return a.ff < b.ff;
+			}
+		});
+		for(int i = 0; i < B.size(); i++){
+			cout << B[i].ff << " " << B[i].ss << endl;
+		}
 	}
 }	
-
-/*
-15 21
-1 2
-1 3 
-2 3
-2 4
-2 6
-3 5
-3 7
-3 8
-4 5
-5 6
-6 7
-8 9
-8 10
-9 10
-9 12
-10 11
-11 12
-12 13
-13 14
-13 15
-14 15
-output : (12 , 13) , (3 , 8)
-*/
-/*
-5 6
-1 2 
-1 3
-2 3
-2 4
-3 5
-4 5
-output : no bridge is present
-*/
