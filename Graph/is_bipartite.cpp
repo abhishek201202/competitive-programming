b2b206bf7915cc51074af2726078a3d7ceb20396
abhishek <abhishekk201202@gmail.com>
2020-07-30 12:23:02 +0530

    Some Frequently used algorithm in competitive programming

--- Graph/is_bipartite.cpp
+++ Graph/is_bipartite.cpp
@@ -9,7 +9,30 @@ int n;
 vector<int> adj[N];
 int col[N];
 
-bool is_bipartite(){
+// Using DFS
+bool is_bipartiteDFS(){
+	bool okk = true;
+	function<void(int, int)> dfs = [&](int u, int p){
+		if(p == -1) col[u] = 1;
+	    else col[u] = col[p] ^ 3;
+	    for(int v : adj[u]){
+	        if(!col[v]){
+	            dfs(v, u);
+	        }else{
+	            if(col[v] == col[u]) okk = false;
+	        }
+	    }
+	};
+	for(int i = 0; i < n; i++){
+		if(!col[i]) dfs(i, -1);
+	}
+	return okk;
+}
+
+
+
+// Using BFS
+bool is_bipartiteBFS(){
 	bool okk = true;
 	for(int i = 0 ; i < n ;i++){
 		if(!col[i]){
