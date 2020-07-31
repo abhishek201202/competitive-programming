bc9a6729febe797a0636f1ac529b274fd624a97f
abhishek <abhishekk201202@gmail.com>
2020-07-31 16:01:50 +0530

    Some Frequently used algorithm in competitive programming

--- Data Structures/dsu.cpp
+++ Data Structures/dsu.cpp
@@ -8,23 +8,32 @@ typedef pair<int,int> pii;
 
 template<typename T>
 struct DSU{
+	int sz;
 	map<T,T> parent;
-	void make_set(T u){
-		parent[u] = u;
+	DSU(){}
+	DSU(int n){
+		this -> sz = n;
+		for(int i = 0; i < n; i++){
+			parent[i] = i;
+		}
 	}
-	T get_parent(T u){
+	T find(T u){
 		if(!parent.count(u)) return parent[u] = u;
 		if(u == parent[u]) return u;
-		return parent[u] = get_parent(parent[u]) ;
+		return parent[u] = find(parent[u]) ;
 	}
 	void merge(T u , T v){
 		if(!parent.count(u)) parent[u] = u;
 		if(!parent.count(v)) parent[v] = v;
-		T p_u = get_parent(u);
-		T p_v = get_parent(v);
+		T p_u = find(u);
+		T p_v = find(v);
 		if(p_v != p_u){
 			parent[p_u] = p_v;
+			sz--;
 		}
+	}
+	int size(){ 
+		return sz;
 	}
 };
 
@@ -33,20 +42,3 @@ int32_t main(){
 	DSU<int> dsu;
 	
 }
-
-/*
-8 2
-3 47
-6 13
-4 47
-7 39
-8 24
-7 23
-7 35
-4 50
-3 2
-5 7
-6 8
-5 6
-240
-*/
