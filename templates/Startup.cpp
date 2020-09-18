7e42b1837b20e2065d4df0068ff77e8abc142bbb
abhishek <abhishekk201202@gmail.com>
2020-09-18 21:06:58 +0530

    Template updated

--- templates/Startup.cpp
+++ templates/Startup.cpp
@@ -1,42 +1,35 @@
-// abhishek soni.....[NSIT'22]
-
 #include <bits/stdc++.h>
-// ~~~~~~~~~~~~~~~~~~~~~> Debugging Zone <~~~~~~~~~~~~~~~~~~~~~~~ //
-template<class T1, class T2>
-std::ostream &operator<<(std::ostream &os, const std::pair<T1,T2> &p){
-	os << '{' << p.first << ", " << p.second << '}';
-	return os;
-}
-template<typename C, 
-typename T = std::decay_t<decltype(*begin(std::declval<C>()))>,
-typename std::enable_if<!std::is_same<C, std::string>::value>::type* = nullptr>
-std::ostream &operator<<(std::ostream &os, const C &container){
-	bool first = true;
-	std::stringstream ss; ss << '[';
-	for(const auto &x : container){
-		if (!first) ss << ", ";
-		first = false; ss << x;
-	}
-	ss << ']';
-	return os << ss.str();
-}
-#define prnt "[Line " << __LINE__ << "] :: "
-#define print(x) "[" << #x << " = " << x << "]"
-#define debugg(x) cerr << prnt << print(x) << endl;
-#define debugg2(x, y) cerr << prnt << print(x) << " " << print(y) << endl;
-#define debugg3(x, y, z) cerr << prnt << print(x) << " " << print(y) << " " << print(z) << endl;
-#define debugg4(x, y, z, w) cerr << prnt << print(x) << " " << print(y) << " " << print(z) << " " << print(w) << endl;
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
-#define endl "\n"
-#define ff first
-#define ss second
-#define int long long
-#define dd long double
-#define pb push_back
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
 using namespace std;
+
+template<class T1, class T2>
+std::ostream &operator<<(std::ostream &os, const std::pair<T1,T2> &p){ os << '{' << p.first << ", " << p.second << '}'; return os;}
+template<typename C,typename T = std::decay_t<decltype(*begin(std::declval<C>()))>,typename std::enable_if<!std::is_same<C,std::string>::value>::type* = nullptr>
+std::ostream &operator<<(std::ostream &os, const C &container){bool first = true; std::stringstream ss; ss << '[';for(const auto &x : container){ if(!first) ss << ", "; first = false; ss << x;} ss << ']';return os << ss.str();}
+vector<string> split(const string& s, char c) { vector<string> v; stringstream ss(s); string x; while (getline(ss, x, c)) v.emplace_back(x); return v;}
+inline void dbg_out(vector<string>::iterator it) { cerr << endl; }
+template<typename T, typename... Args>
+inline void dbg_out(vector<string>::iterator it, T a, Args... args){ cerr << "[" << it->substr((*it)[0] == ' ', it->length()) << " = " << a << "] "; dbg_out(++it, args...); }
+#define debugg(...) {cerr << "[Line " << __LINE__ << "] :: "; dbg_out(split(#__VA_ARGS__, ',').begin(), __VA_ARGS__);}
+
+#define endl             "\n"
+#define ff               first
+#define ss               second
+#define int              long long
+#define dd               long double
+#define pb               push_back
+#define mp               make_pair
+#define ALL(x)           x.begin(), x.end()
+#define FOR(i, s, e)     for(int i = s; i <= e; i++)
+#define FORR(i, s, e)    for(int i = s; i >= e; i--)
+
 typedef pair<int, int> pii;
 typedef pair<int, pair<int, int>> piii;
-
+template<typename T> 
+using pbds = tree<T, null_type, less<T>, 
+rb_tree_tag ,tree_order_statistics_node_update>; 
 
 const int mod = 1e9 + 7;
 const int MOD = 998244353;
@@ -44,16 +37,18 @@ const int N   = 5e5 + 5;
 
 
 void test_cases(){
-	
-
+    
 }
+
 
 int32_t main(){
     // ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
-	int tt = 1;
-	cin >> tt;
-	for(int T = 1; T <= tt; T++){
-		cerr << endl << "Case #" << T << ": " << endl;
-		test_cases();
-	}
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    int tt = 1;
+    cin >> tt;
+    for(int T = 1; T <= tt; T++){
+        cerr << endl << "Case #" << T << ": " << endl;
+        test_cases();
+    }
 }
