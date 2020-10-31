9ceab73ada7a8c5b8a2c7c29f7dd73b908e39923
abhishek <abhishekk201202@gmail.com>
2020-10-31 13:57:58 +0530

    STL tricks

---
+++ STL tricks/set.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// custom comparator for set
+struct cmp{
+    bool operator() (const int& lhs, const int& rhs) const {
+        return lhs > rhs;
+    }
+};
+
+int32_t main(){
+    set<int, cmp> st;
+    for(int i = 0; i < 10; i++){
+        st.insert(i);
+    }
+    for(int x : st){
+        cout << x << " ";
+    }
+    cout << endl;
+}
