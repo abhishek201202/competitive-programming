823e0c62b1ab96f5342b4d0a5aa5f4d370a11411
abhishek <abhishekk201202@gmail.com>
2021-04-14 09:41:27 +0530

    updated

--- Maths/Prime_factors_upto_1e18.cpp
+++ Maths/Prime_factors_upto_1e18.cpp
@@ -1,35 +1,10 @@
 // https://codeforces.com/gym/102861/problem/N
 #include <bits/stdc++.h>
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
-using namespace __gnu_pbds;
+#define int long long
+#define ALL(x) x.begin(), x.end()
 using namespace std;
-
-template<class T1, class T2> ostream &operator<<(ostream &os, const pair<T1,T2> &p){ os << '{' << p.first << ", " << p.second << '}'; return os;}
-template<typename C,typename T = decay_t<decltype(*begin(declval<C>()))>,typename enable_if<!is_same<C,string>::value>::type* = nullptr>
-ostream &operator<<(ostream &os, const C &container){bool first = true; stringstream ss; ss << '[';for(const auto &x : container){ if(!first) ss << ", "; first = false; ss << x;} ss << ']';return os << ss.str();}
-vector<string> split(const string& s, char c){ vector<string> v; stringstream ss(s); string x; while (getline(ss, x, c)) v.emplace_back(x); return v;}
-inline void dbg_out(vector<string>::iterator it){ cerr << endl;}template<typename T, typename... Args> inline void dbg_out(vector<string>::iterator it, T a, Args... args){ cerr << "[" << it->substr((*it)[0] == ' ', it->length()) << " = " << a << "] "; dbg_out(++it, args...); }
-inline void READ_OUT(vector<string>::iterator it){}template<typename T, typename... Args> inline void READ_OUT(vector<string>::iterator it, T& a, Args&... args){cin >> a;READ_OUT(++it, args...);}
-#define debugg(...) {cerr << "[Line " << __LINE__ << "] :: "; dbg_out(split(#__VA_ARGS__, ',').begin(), __VA_ARGS__);}
-#define READ(...) READ_OUT(split(#__VA_ARGS__, ',').begin(), __VA_ARGS__);
-
-#define       endl             "\n"
-#define       ff               first
-#define       ss               second
-#define       int              long long
-#define       dd               long double
-#define       ALL(x)           x.begin(), x.end()
-#define       FOR(i, s, e)     for(int i = s; i <= e; i++)
-#define       FORR(i, s, e)    for(int i = s; i >= e; i--)
-#define       getcurrtime      ((double)clock()/CLOCKS_PER_SEC)
-template<typename T> using pbds = tree<T, null_type, less<T>, rb_tree_tag ,tree_order_statistics_node_update>; 
-
 const int mod = 1e9 + 7;
-const int MOD = 998244353;
-const int N   = 2e6 + 5;
-
-
+const int N = 2e6 + 5;
 
 vector<int> Prime;
 void sieve(){
@@ -46,30 +21,28 @@ void sieve(){
 }
 
 
-int modMint(int a, int b, const int mod) {
-    int ret = a*b-mod*(int)((long double)a*b/mod);
-    return ret+((ret<0)-(ret>=(int)mod))*mod; }
-
-// int modMint(int a, int b, int mod){
-// 	a = (a%mod + mod)%mod;
-// 	b = (b%mod + mod)%mod;
-// 	return (a * b)%mod;
-// }
-
-int modPow(int a, int b, const int mod) {
-    if (b == 0) return 1;
-    int res = modPow(a,b/2,mod); res = modMint(res,res,mod);
-    return b&1 ? modMint(res,a,mod) : res;
+int mul(int a, int b, int mod){
+    int ret = a * b - mod * (int)((long double)a * b / mod);
+    ret = ret + (ret < 0 ? mod : 0);
+    return ret;
 }
+
+int pow(int a, int b, const int mod) {
+    if (b == 0) return 1;
+    int res = pow(a,b/2,mod); 
+    res = mul(res, res, mod);
+    return b&1 ? mul(res,a,mod) : res;
+}
+
+
 bool isPrime(int n) { // returns true if n is probably prime, else returns false.
-    if (n < 2 || n % 6 % 4 != 1) {
-    	return n-2 < 2;
-    }
-    int A[] = {2, 325, 9375, 28178, 450775, 9780504, 1795265022},
-        s = __builtin_ctzll(n-1), d = n>>s;
+    if(n < 2 || n % 6 % 4 != 1) return n-2 < 2;
+    int A[] = {2, 325, 9375, 28178, 450775, 9780504, 1795265022};
+    int s = __builtin_ctzll(n-1);
+    int d = n >> s;
     for(int a : A) {   // ^ count trailing zeroes
-        int p = modPow(a,d,n), i = s;
-        while (p != 1 && p != n-1 && a%n && i--) p = modMint(p,p,n);
+        int p = pow(a,d,n), i = s;
+        while (p != 1 && p != n-1 && a%n && i--) p = mul(p,p,n);
         if (p != n-1 && i != s) return 0;
     }
     return 1;
@@ -78,7 +51,7 @@ bool isPrime(int n) { // returns true if n is probably prime, else returns false
 
 /* method to return prime divisor for n */
 int PollardRho(int n){
-    auto f = [n](int x) { return (modMint(x,x,n)+1)%n; };
+    auto f = [n](int x) { return (mul(x,x,n)+1)%n; };
     if (!(n&1)) return 2;
     for (int i = 2;;++i) {
         int x = i, y = f(x), p;
@@ -90,6 +63,8 @@ int PollardRho(int n){
     }
 }
 
+
+// Prime Factors upto 1e18
 vector<int> PrimeFactors(int n){
 	vector<int> res;
 	for(int p : Prime){
@@ -112,15 +87,8 @@ vector<int> PrimeFactors(int n){
 }
 
 
-
-
-
-int32_t main(){
-    ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
-    sieve();
-
-
-    int m, n, k; cin >> m >> n >> k;
+void test_cases(){
+	int m, n, k; cin >> m >> n >> k;
 	vector<int> a(n);
 	for(int i = 0; i < n; i++){
 		cin >> a[i];
@@ -143,4 +111,18 @@ int32_t main(){
 		cout << x << " ";
 	}
 	cout << endl;
+}
+
+
+int32_t main(){
+    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    int tt = 1;
+    // cin >> tt;
+    sieve();
+    for(int T = 1; T <= tt; T++){
+        // cerr << endl << "Case #" << T << ": " << endl;
+        test_cases();
+    }
 }
--- STL tricks/ModularClass.cpp
+++ STL tricks/ModularClass.cpp
@@ -5,20 +5,38 @@ struct Mint{
     int64_t x;
     static const int64_t mod = 1e9 + 7;
     Mint(int64_t x = 0) : x((x%mod + mod)%mod){}
+    // a % b = (a — b * (a / b))
+    // % operator is costly 
+    Mint& operator+=(const Mint a){ 
+        x = (x + a.x) - mod * (int)((long double)(x + a.x) / mod);
+        x = x + (x < 0 ? mod : 0);
+        return *this; 
+    }
+    Mint& operator-=(const Mint a){ 
+        x = (x - a.x) - mod * (int)((long double)(x - a.x) / mod);
+        x = x + (x < 0 ? mod : 0);
+        return *this;
+    }
+    Mint& operator*=(const Mint a){ 
+        x = x * a.x - mod * (int)((long double)x * a.x / mod);
+        x = x + (x < 0 ? mod : 0);
+        return *this;
+    }
+
     Mint pow(int64_t b) const{ if(!b) return Mint(1); Mint a = pow(b >> 1); a *= a; if(b&1) a *= *this;return a; }
     Mint inv() const{ return pow(mod - 2); }
-    Mint& operator+=(const Mint a){ x = (x + a.x%mod + mod)%mod; return *this; }
-    Mint& operator-=(const Mint a){ x = (x - a.x%mod + mod)%mod;return *this; }
-    Mint& operator*=(const Mint a){ x = (x * (a.x%mod + mod))%mod;return *this;}
+    
     Mint& operator/=(const Mint a){ return (*this) *= a.inv(); }
     Mint operator+(const Mint a) const{ return Mint(*this) += a; }
     Mint operator-(const Mint a) const{ return Mint(*this) -= a; }
     Mint operator*(const Mint a) const{ return Mint(*this) *= a; }
     Mint operator/(const Mint a) const{ return Mint(*this) /= a; }
+    
     friend bool operator==(const Mint a, const Mint b){ return a.x == b.x; }
     friend bool operator!=(const Mint a, const Mint b){ return a.x != b.x; }
     friend ostream& operator<<(ostream& os, Mint const& a){ return os << a.x; }
 };
+
 
 
 int32_t main(){
