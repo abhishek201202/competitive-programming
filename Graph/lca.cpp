b93d119b2174defb715ca7656ce19b740f899a08
abhishek <abhishekk201202@gmail.com>
2021-04-21 13:32:26 +0530

    updated

--- Graph/lca.cpp
+++ Graph/lca.cpp
@@ -8,68 +8,78 @@ typedef pair<int,int> pii;
 
 
 // indexing of tree is from ( 0 to n - 1 ) ........
- 
+struct LCA{
+	int n, H = 32;
+	vector<vector<int>> adj, parent;
+	vector<int> depth;
 
-const int H = 32;
-const int N = (int)2e5 + 5;
-vector<int> adj[N];
-int parent[N][H] , depth[N];
-
-// dfs used to set parent of a node, depth of a node, subtree size of a node
-void dfs(int u , int par = -1){
-	if(par != -1){ 
-		parent[u][0] = par; // just usse 1 height uppar walla
-		depth[u] = depth[par] + 1;
+	LCA(int _n = 0){
+		init(_n);
 	}
-	for(auto v : adj[u]){
-		if(v == par) continue;
-		dfs(v , u);
-	}
-}
+	LCA(vector<vector<int>>& _adj){
+		init(_adj.size());
+		adj = _adj;
+	} 
 
-// precompute all parent of all node which present at height of 2^i
-// if 2^i th parent is not reachable then place -1
- 
-void lca_pre(int n){
-	for(int i = 1; i < H ; i++){
-		for(int node = 0 ; node < n ; node++){
-			if(parent[node][i - 1] == -1) continue;
-			parent[node][i] = parent[parent[node][i - 1]][i - 1];
+	void init(int sz){
+		adj = vector<vector<int>>(sz, vector<int>());
+		parent = vector<vector<int>>(sz, vector<int>(H, 0));
+		depth = vector<int>(sz, 0);
+	}
+
+	void add_edge(int u, int v){
+		adj[u].push_back(v);
+		adj[v].push_back(u);
+	}
+
+
+	void build(){
+		function<void(int, int)> dfs = [&](int u, int par = -1){
+			if(par != -1){ 
+				parent[u][0] = par;
+				depth[u] = depth[par] + 1;
+			}
+			for(auto v : adj[u]){
+				if(v == par) continue;
+				dfs(v , u);
+			}
+		};
+		dfs(0, -1);
+		for(int i = 1; i < H ; i++){
+			for(int node = 0; node < n; node++){
+				if(parent[node][i - 1] == -1) continue;
+				parent[node][i] = parent[parent[node][i - 1]][i - 1];
+			}
 		}
 	}
-}
 
-
-// to get lca of two node u and v
- 
-int lca(int u , int v){
-	if(depth[u] > depth[v]) swap(u , v);
-	int diff = depth[v] - depth[u];
-	for(int i = 0 ; i < H ; i++){
-		if((diff >> i) & 1) v = parent[v][i];
-	}
-	if(u == v) return u;
-	for(int i = H - 1 ; i >= 0 ; i--){
-		if(parent[u][i] == -1 || parent[v][i] == -1) continue;
-		if(parent[u][i] != parent[v][i]){
-			u = parent[u][i];
-			v = parent[v][i];
+	int lca(int u , int v){
+		if(depth[u] > depth[v]) swap(u , v);
+		int diff = depth[v] - depth[u];
+		for(int i = 0 ; i < H ; i++){
+			if((diff >> i) & 1) v = parent[v][i];
 		}
+		if(u == v) return u;
+		for(int i = H - 1 ; i >= 0 ; i--){
+			if(parent[u][i] == -1 || parent[v][i] == -1) continue;
+			if(parent[u][i] != parent[v][i]){
+				u = parent[u][i];
+				v = parent[v][i];
+			}
+		}
+		return parent[u][0];
 	}
-	// now we are just one hoop below the lca
-	return parent[u][0];
-}
+};
+
+
 int32_t main(){
 	ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
 	int n, m ; cin >> n >> m;
+	LCA lca(n);
 	for(int i =0 ; i  < n -1 ; i++){
 		int s ,d ; cin >> s >> d;
-		adj[s - 1].pb(d - 1);
-		adj[d - 1].pb(s - 1);
+		lca.add_edge(s - 1, d - 1);
 	}
-	memset(parent , -1 , sizeof(parent));
-	dfs(0);
-	lca_pre(n);
-	
+	lca.build();
 
 }
--- Maths/is_prime.cpp
+++ Maths/is_prime.cpp
@@ -1,50 +1,60 @@
+// https://www.spoj.com/problems/PON/
 #include<bits/stdc++.h>
-#define int long long
-#define ff first
-#define ss second
-#define pb push_back
+#define int long long int
 using namespace std;
 
-bool trial_division(int n){
-	for(int i = 2; i <= sqrt(n) ; i++){
-		if(n%i == 0) return false;
-	}
-	return true;
+// passed all the test case in Google Codejam Round B 2021 
+// Consecutive Primes
+// working good for n <= 1e13 on spoj
+bool miller_rabin(int n) { // returns true if n is probably prime, else returns false.
+    if(n < 2 || n % 6 % 4 != 1) return n - 2 < 2;
+    auto power = [&](uint64_t a, uint64_t b, uint64_t mod){
+        uint64_t res = 1; a %= mod;
+        while(b){
+            if(b&1ll) res = (__uint128_t)res * a % mod;
+            a = (__uint128_t)a * a % mod;
+            b >>= 1;
+        }
+        return res;
+    };
+    int64_t A[] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37};
+    int s = __builtin_ctzll(n-1);
+    int d = n >> s;
+    for(int64_t a : A) {   // ^ count trailing zeroes
+        int p = power(a, d, n), i = s;
+        while (p != 1 && p != n-1 && a%n && i--) p = (p * p) % n;
+        if (p != n-1 && i != s) return false;
+    }
+    return true;
+}
+
+// Didn't passed all the test case in Google Codejam Round B 2021 
+// Consecutive Primes
+// working good for n <= 2 ^ 64 on spoj
+bool probablyPrimeFermat(int n, int iter = 5) {
+    if(n < 4) return n == 2 || n == 3;
+    auto power = [&](uint64_t a, uint64_t b, uint64_t mod){
+        uint64_t res = 1; a %= mod;
+        while(b){
+            if(b&1ll) res = (__uint128_t)res * a % mod;
+            a = (__uint128_t)a * a % mod;
+            b >>= 1;
+        }
+        return res;
+    };
+    for(int i = 0; i < iter; i++) {
+        int a = 2 + rand() % (n - 3);
+        if(power(a, n - 1, n) != 1) return false;
+    }
+    return true;
 }
 
 
-bool fermat_primality(int n){
-	if(n < 4) return n == 2 || n == 3;
-	auto pow = [&](int a , int b , int m)->int{
-		if(a == 0) return 0;
-		int res = 1ll;
-		a %= m;
-		while(b > 0){
-			if(b & 1ll){
-				res = (res * a) % m;
-			}
-			a = (a * a) % m;
-			b >>= 1ll;
-		}
-		return res;
-	};
-	int cnt1 = 0 , cnt0 = 0;
-	for(int p = 0 ; p < 1e6 ; p++){
-		int a = 2 + rand() % (n - 3);
-		if(pow(a , n - 1 , n) != 1){
-			cnt0 ++;
-		}else{
-			cnt1++;
-		}
-	}
-	return (cnt1 >= cnt0) ? true : false;
-}
 
 int32_t main(){
-	while(1){
-		cout << endl << endl << "new test case ..........." << endl;
+	int tt; cin >> tt;
+	while(tt--){
 		int n; cin >> n;
-		cout << "according to trial division   : " << trial_division(n) << endl;
-		cout << "according to fermat_primality : " << fermat_primality(n) << endl;
-	}
+		cout << (probablyPrimeFermat(n) ? "YES" : "NO") << endl;
+    }
 }
