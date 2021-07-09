5ecaecc034cdfcf252df8701c6e79ef547c7835c
abhishek <abhishekk201202@gmail.com>
2021-07-09 09:51:12 +0530

    update

--- Data Structures/tries.cpp
+++ Data Structures/tries.cpp
@@ -1,87 +1,103 @@
-#include<bits/stdc++.h>
-#define int long long
-#define ff first
-#define ss second
-#define pb push_back
-using namespace std;
+struct BitTrie{
+    const int B = 32;
+    BitTrie *left, *right;
+    int cnt;
+    BitTrie(){
+        left = right = NULL;
+        cnt = 0;
+    }
 
-const int B = 32;
-const int N = 1e7;
-int arr[N];
-int n ;
-struct trieNode{
-	trieNode *left , *right;
+    // insert a number into the trie
+    void insert(int n){
+        BitTrie *root = this;
+        for(int bit = B - 1; bit >= 0; bit--){
+            int x = n & (1ll << bit);
+            if(x){
+                if(root -> right == NULL){
+                    root -> right = new BitTrie();
+                }
+                root = root -> right;
+            }else{
+                if(root -> left == NULL){
+                    root -> left = new BitTrie();
+                }
+                root = root -> left;
+            }
+            root -> cnt += 1;
+        }
+    }
+
+    // count of element whose xor with n < k
+    int countSmallerPairs(int k, int n){
+        BitTrie *root = this;
+        int res = 0;
+        for(int bit = B - 1; bit >= 0 && root; bit--){
+            int x = n & (1ll << bit);
+            int y = k & (1ll << bit);
+            if(y == 0){
+                if(x){
+                    root = root -> right;
+                }else{
+                    root = root -> left;
+                }
+            }else{
+                if(x){
+                    if(root -> right != NULL){
+                        res += root -> right -> cnt;
+                    }
+                    root = root -> left;
+                }else{
+                    if(root -> left != NULL){
+                        res += root -> left -> cnt;
+                    }
+                    root = root -> right;
+                }
+            }
+        }
+        return res;
+    }
+
+
+
+    // find max xor pair
+    int find_max_pair(int ele){
+        BitTrie *root = this;
+        int res = 0;
+        for(int bit = B - 1; bit >= 0; bit--){
+            int x = ele & (1ll << bit);
+            if(x){
+                if(root->left != NULL){
+                    res += (1ll << bit);
+                    root = root->left;
+                }else{
+                    root = root->right;
+                }
+            }else{
+                if(root->right != NULL){
+                    res += (1ll << bit);
+                    root = root->right;
+                }else{
+                    root = root->left;
+                }
+            }
+        }
+        return res;
+    }
+
+
+
+    // find max xor pair
+    int max_xor_subarray(vector<int> &a){
+        BitTrie *root = new BitTrie();
+        int ans = a[0];
+        int curr_xor = ans;
+        insert(ans);
+        for(int i = 1; i < a.size(); i++){
+            curr_xor ^= a[i];
+            int temp_ans = find_max_pair(curr_xor);
+            ans = max(ans , temp_ans);
+            insert(curr_xor);
+        }
+        return ans;
+    }
 };
-
-// maximum xor subarray
-
-void insert(trieNode *head , int ele){
-	trieNode *curr = head;
-	for(int i = B - 1 ; i >= 0 ; i--){
-		int bit = ele & (1ll << i);
-		if(bit){
-			if(curr->right != NULL){
-				curr = curr->right;
-			}else{
-				curr->right = new trieNode();
-				curr = curr->right;
-			}
-		}else{
-			if(curr->left != NULL){
-				curr = curr->left;
-			}else{
-				curr->left = new trieNode();
-				curr = curr->left;
-			}
-		}
-	}
-}
-
-int find_max_pair(trieNode *head , int ele){
-	trieNode *curr = head;
-	int ans = 0;
-	for(int i = B - 1 ;i >= 0 ;i--){
-		int bit = ele & (1ll << i);
-		if(bit){
-			if(curr->left != NULL){
-				ans += (1ll << i);
-				curr = curr->left;
-			}else{
-				curr = curr->right;
-			}
-		}else{
-			if(curr->right != NULL){
-				ans += (1ll << i);
-				curr = curr->right;
-			}else{
-				curr = curr->left;
-			}
-
-		}
-	}
-	return ans;
-}
-
-int max_xor_subarray(){
-	trieNode *head = new trieNode();
-	int ans = arr[0];
-	int curr_xor = ans;
-	insert(head , ans);
-	for(int i = 1; i < n ; i++){
-		curr_xor ^= arr[i];
-		int temp_ans = find_max_pair(head , curr_xor);
-		ans = max(ans , temp_ans);
-		insert(head , curr_xor);
-	}
-	return ans;
-}
-
-
-int32_t main(){
-	cin >> n;
-	for(int i = 0 ;i < n ;i++){
-		cin >> arr[i];
-	}
-	cout << max_xor_subarray() << endl;
-
-}
