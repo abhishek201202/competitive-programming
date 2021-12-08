54a6cf57854e30a2bc4c4f6af25e461d307d761c
abhishek <abhishekk201202@gmail.com>
2021-12-08 21:34:38 +0530

    update

--- Data Structures/segment_tree_lazy_propagation.cpp
+++ Data Structures/segment_tree_lazy_propagation.cpp
@@ -6,59 +6,99 @@
 using namespace std;
 
 
-// horrible queries.....[coding ninjas]
+
+
+struct Node{
+    int s;
+    Node(){
+        s = 0;
+    }
+    void update(int val){ // node update
+        s += val;
+    }
+    Node operator+(const Node &v) const{ // node merge 
+        Node res;
+        res.s = s + v.s;
+        return res;
+    }
+};
+
+struct Lazy{
+    int val;
+    Lazy(){
+        val = 0;
+    }
+    void update(int val){
+        this -> val += val;
+    }
+};
+
 struct SegmentTree{
     int n;
-    vector<int> a, tree, lazy;
+    vector<int> a;
+    vector<Node> T;
+    vector<Lazy> L;
+
     SegmentTree(vector<int> &a){
         this -> a = a;
         n = a.size();
-        tree.resize(4 * n, 0);
-        lazy.resize(4 * n, 0);
+        T.resize(4 * n);
+        L.resize(4 * n);
     }
 
-    void pushdown(int node, int si , int ei){
-        if(lazy[node] != 0){
-            tree[node] += (ei - si + 1) * lazy[node];
+    void pushdown(int root, int si, int ei){
+        if(L[root].val != 0){
+            T[root].update((ei - si + 1) * L[root].val);
             if(si != ei){
-                lazy[2 * node] += lazy[node];
-                lazy[2 * node + 1] += lazy[node];
+                L[2 * root].update(L[root].val);
+                L[2 * root + 1].update(L[root].val);
             }
-            lazy[node] = 0;
+            L[root].val = 0;
         }
     }
+    
+    void build(int root, int si, int ei){
+        if(si == ei){ T[root].update(a[si]); return; }
+        int mid = (si + ei)/2;
+        build(2 * root, si, mid);
+        build(2 * root + 1, mid + 1, ei);
+        T[root] = T[2 * root] + T[2 * root + 1];
+    }
+    void build() { build(1, 0, n - 1); }
 
-    void update(int l, int r, int val, int node, int si , int ei){
-        pushdown(node, si , ei);     // previous updation
-        if(l > ei || r < si) return; // no overlapping
-        if(l <= si && r >= ei){      // complete overlapping
-            tree[node] += (ei - si + 1) * val;
+    Node qry(int l, int r, int root, int si, int ei){
+        if(l > ei || r < si) return Node();
+        pushdown(root, si, ei);
+        if(l <= si && r >= ei) return T[root];
+        int mid = (si + ei)/2;
+        Node L = qry(l, r, 2 * root, si, mid);
+        Node R = qry(l, r, 2 * root + 1, mid + 1, ei);
+        return L + R;
+    }
+    int qry(int l, int r) { return qry(l, r, 1, 0, n - 1).s;}
+
+    void update(int l, int r, int val, int root, int si, int ei){
+        pushdown(root, si, ei);
+        if(l > ei || r < si) return;
+        if(l <= si && r >= ei){
+            T[root].update((ei - si + 1) * val); // change acc. to Qns
             if(si != ei){
-                lazy[2 * node] +=  val;
-                lazy[2 * node + 1] +=  val;
+                L[2 * root].update(val);
+                L[2 * root + 1].update(val);
             }
             return;
         }
-        // partial overlap
-        int mid = (si + ei)/2;
-        update(l, r, val, 2 * node, si , mid);
-        update(l, r, val, 2 * node + 1, mid + 1, ei);
-        tree[node] = tree[2 * node] + tree[2 * node + 1];
+        int mid = (si + ei) / 2;
+        update(l, r, val, 2 * root, si, mid);
+        update(l, r, val, 2 * root + 1, mid + 1 , ei);
+        T[root] = T[2 * root] + T[2 * root + 1];
     }
     void update(int l, int r, int val){ update(l, r, val, 1, 0, n - 1);}
-
-
-    int qry(int l, int r, int node, int si, int ei){
-        if(l > ei || r < si) return 0;
-        pushdown(node, si, ei);
-        if(l <= si && r >= ei) return tree[node];
-        int mid = (si + ei) / 2;
-        int L = qry(l, r, 2 * node, si, mid);
-        int R = qry(l, r, 2 * node + 1, mid + 1, ei);
-        return L + R;
-    }
-    int qry(int l, int r){ return qry(l, r, 1, 0, n - 1);}
 };
+
+
+
+
 
 int32_t main(){
     int t = 1;
