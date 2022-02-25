7fa39ad64ecc0f506228032e1a00fa36d5f79a2c
abhishek <abhishekk201202@gmail.com>
2022-02-25 11:37:18 +0530

    added

--- Coding Competition/HashCode/HashCode.cpp
+++ Coding Competition/HashCode/HashCode.cpp
@@ -1,50 +1,47 @@
-/**
- * author: abhishek soni ..... [NSIT'22]
- * created: 22/02/2022  19:35:33
- */
 #include <bits/stdc++.h>
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
-using namespace __gnu_pbds;
 using namespace std;
 
-template<class T1, class T2> ostream &operator<<(ostream &os, const pair<T1,T2> &p){ os << '{' << p.first << ", " << p.second << '}'; return os;}
-template<typename C,typename T = decay_t<decltype(*begin(declval<C>()))>,typename enable_if<!is_same<C,string>::value>::type* = nullptr>
-ostream &operator<<(ostream &os, const C &container){bool first = true; stringstream ss; ss << '[';for(const auto &x : container){ if(!first) ss << ", "; first = false; ss << x;} ss << ']';return os << ss.str();}
-vector<string> split(const string& s, char c){ vector<string> v; stringstream ss(s); string x; while (getline(ss, x, c)) v.emplace_back(x); return v;}
-inline void dbg_out(vector<string>::iterator it){ cerr << endl;}template<typename T, typename... Args> inline void dbg_out(vector<string>::iterator it, T a, Args... args){ cerr << "[" << it->substr((*it)[0] == ' ', it->length()) << " = " << a << "] "; dbg_out(++it, args...); }
-inline void READ_OUT(vector<string>::iterator it){}template<typename T, typename... Args> inline void READ_OUT(vector<string>::iterator it, T& a, Args&... args){cin >> a;READ_OUT(++it, args...);}
-template<typename T> inline void debugg2D(T a, int n, int m){for(int i = 0; i < n; i++){for(int j = 0; j < m; j++){cout << a[i][j] << " ";}cout << endl;}}
-#define debugg(...) {cerr << "[Line " << __LINE__ << "] :: "; dbg_out(split(#__VA_ARGS__, ',').begin(), __VA_ARGS__);}
-#define READ(...) READ_OUT(split(#__VA_ARGS__, ',').begin(), __VA_ARGS__);
-
-#define endl             "\n"
-#define ff               first
-#define ss               second
-#define vt               vector
-#define ar               array
-#define int              long long
-#define dd               long double
-#define ALL(x)           x.begin(), x.end()
-#define FOR(i, s, e)     for(int i = s; i <= e; i++)
-#define FORR(i, s, e)    for(int i = s; i >= e; i--)
-#define getcurrtime      ((double)clock()/CLOCKS_PER_SEC)
-mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
-template<typename T> using pbds = tree<T, null_type, less<T>, rb_tree_tag ,tree_order_statistics_node_update>; 
-
-const array<int, 2> dir4[4] = {{0, -1}, {-1, 0}, {0, +1}, {+1, 0}};
-const array<int, 2> dir8[8] = {{0, -1}, {-1, -1}, {-1, 0}, {-1, +1}, {0, +1}, {+1, +1}, {+1, 0}, {+1, -1}};
-const array<int, 2> dirD[4] = {{-1, -1}, {-1, +1}, {+1, +1}, {+1, -1}};
-const array<int, 2> dirK[8] = {{-1, -2}, {-2, -1}, {-2, +1}, {-1, +2}, {+1, +2}, {+2, +1}, {+2, -1}, {+1, -2}};
-
-const int mod = 1e9 + 7;
-const int MOD = 998244353;
-const int N   = 5e5 + 5;
 
 /*=================================================================
 Global Variables Start 
 =================================================================*/
+int C, P;
 
+struct Contributor{
+    string name;
+    int n;
+    unordered_map<string, int> skills;
+    void read(){
+        cin >> name >> n;
+        for(int i = 0; i < n; i++){
+            string t; int val; cin >> t >> val;
+            skills[t] = val;
+        }
+    }
+};
+
+struct Project{
+    string name;
+    int D, S, B, R, L = -1e9;
+    vector<string> RoleName;
+    vector<int> RoleLevel;
+    void read(){
+        cin >> name >> D >> S >> B >> R;
+        for(int i = 0; i < R; i++){
+            string t; int val; cin >> t >> val;
+            RoleName.push_back(t);
+            RoleLevel.push_back(val);
+            L = max(L, val);
+        }
+    }
+};
+
+vector<Contributor> contributors;
+vector<Project> projects;
+
+vector<vector<string>> resC;
+vector<string> resP;
+int FinalScore = 0;
 
 /*=================================================================
 Global Variables End
@@ -53,60 +50,173 @@ Global Variables End
 
 // take all input here only
 void takeInput(){
-
+    
 }
 
 
 // print here whatever you have to print for final answer
 void outputResult(int score = 0){ 
-    
+    cout << resP.size() << endl;
+    for(int i = 0; i < resP.size(); i++){
+        cout << resP[i] << endl;
+        for(int j = 0; j < resC[i].size(); j++){
+            cout << resC[i][j] << " ";
+        }
+        cout << endl;
+    }
 }
 
-string createNewFile(int score = 0){
-    fstream file;
-    string fileName = "out" + to_string(score) + ".txt";
-    file.open(fileName, ios::out);
-    file.close();
-    return fileName;
-}
+// string createNewFile(int score = 0){
+//     fstream file;
+//     string fileName = "out" + to_string(score) + ".txt";
+//     file.open(fileName, ios::out);
+//     file.close();
+//     return fileName;
+// }
 
-void outputInFile(int score = 0){
-    string s = createNewFile(score);
-    char *fileName = new char[s.size() + 1];
-    strcpy(fileName, s.c_str());
-    freopen(fileName, "w", stdout);
-    outputResult(score);
-}
+// void outputInFile(int score = 0){
+//     string s = createNewFile(score);
+//     char *fileName = new char[s.size() + 1];
+//     strcpy(fileName, s.c_str());
+//     freopen(fileName, "w", stdout);
+//     outputResult(score);
+// }
 
-// score given by hashcode system
-int findScore(){
+// // score given by hashcode system
+// int findScore(){
+//     return FinalScore;
+// }
 
-}
+/*
+1) increasing Lk
+2) B/D min
+3) score max
 
-void solve(){
-    
-}
+*/
 
 
-void test_cases(){
-    takeInput();
-    solve();
-    outputInFile(findScore());
+void solve(){ // logic
+
+    sort(projects.begin(), projects.end(), [&](auto &a, auto &b){
+        // return a.L - 1.25* a.S + 2*a.B < b.L - 1.25 * b.S + 2*b.B;
+        // return a.L - 1.5987 * a.S + 0.78 * a.B + a.D < b.L - 1.5987 * b.S + 0.78 * b.B + a.D;
+        // return 1.5987 * a.S  - 0.53783 * a.L >  1.5987 * b.S - 0.53783 * a.L;
+        // return a.S > b.S;
+        return 3.835638 * a.L - a.S * a.S < 3.835638 * a.L - b.S * b.S;
+        // return a.B < b.B;
+        // return 1.893456 * a.S 0.78 * a.B > 1.893456 * b.S + 0.78 * a.B;
+    });
+
+
+    int Time = 0;
+
+    // for(Project &currProject: projects){
+    for(int kk = 0; kk < projects.size();){
+        vector<int> candidateTaken(C, 0);
+        int j = kk;
+        int CurrT = Time;
+        for(; j < projects.size(); j++){
+            Project currProject = projects[j];
+
+            int currScore = currProject.S - (Time + currProject.D - currProject.B);
+            if(currScore <= 0){
+                continue;
+            }
+            if(Time + currProject.D <= currProject.B){
+                currScore = currProject.S;
+            }
+
+            vector<string> &RoleName = currProject.RoleName;
+            vector<int> &RoleLevel = currProject.RoleLevel;
+            vector<pair<int, vector<int>>> possibleCandidate(RoleName.size());
+            int k = 0;
+            for(int i = 0; i < RoleName.size(); i++){
+                string topic = RoleName[i];
+                int level = RoleLevel[i];
+                vector<int> tmp;
+                for(int i = 0; i < C; i++){
+                    if(contributors[i].skills[topic] >= level){
+                        tmp.push_back(i);
+                    }
+                }
+                possibleCandidate[k] = {k, tmp};
+                k++;
+            }
+
+            sort(possibleCandidate.begin(), possibleCandidate.end(), [&](auto &a, auto &b){
+                return a.second.size() < b.second.size();
+            });
+
+            vector<int> candidateChoosen(RoleName.size(), 0);
+            for(int i = 0; i < RoleName.size(); i++){
+                for(int candIdx: possibleCandidate[i].second){
+                    if(candidateTaken[candIdx] == 0){
+                        int role = possibleCandidate[i].first;
+                        candidateChoosen[role] = candIdx;
+                        candidateTaken[candIdx] = 1;
+                        break;
+                    }
+                }
+            }
+
+            bool isAssigned = true;
+            for(int i = 0; i < RoleName.size(); i++){
+                if(candidateChoosen[i] == 0){
+                    isAssigned = false;
+                    break;
+                }
+            }
+
+            if(isAssigned){
+                for(int i = 0; i < RoleName.size(); i++){
+                    int role = i;
+                    int candIdx = candidateChoosen[i];
+                    string name = currProject.RoleName[role];
+                    if(contributors[candIdx].skills[name] == currProject.RoleLevel[role]){
+                        contributors[candIdx].skills[name] += 1;
+                    }
+                }
+            }
+
+
+            if(isAssigned){
+                FinalScore += currScore;
+                // Time += currProject.D;
+                CurrT = max(CurrT, Time + currProject.D);
+            }
+
+            if(isAssigned){
+                resP.push_back(currProject.name);
+                resC.push_back(vector<string>());
+                for(int &candIdx: candidateChoosen){
+                    resC.back().push_back(contributors[candIdx].name);
+                }
+            }
+
+            if(isAssigned == 0){
+                break;
+            }
+        }
+        if(j == kk) kk++;
+        else kk = j;
+        Time = CurrT;
+    }
+    outputResult();
 }
 
 
 int32_t main(){
-    // ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
-    // freopen("in1.txt", "r", stdin);
-    int tt = 1;
-    cin >> tt;
-    for(int T = 1; T <= tt; T++){
-        cerr << endl << "Case #" << T << ": " << endl;
-        test_cases();
+    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+    freopen("in6.txt", "r", stdin);
+    freopen("out6.txt", "w", stdout);
+    cin >> C >> P;
+    contributors = vector<Contributor>(C);
+    projects = vector<Project>(P);
+    for(int i = 0; i < C; i++){
+        contributors[i].read();
     }
+    for(int i = 0; i < P; i++){
+        projects[i].read();
+    }
+    solve();
 }
-
-
-
-
-
